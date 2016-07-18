78f7ddea8c4ebc22e379d91b713ad6fe5c278a5f
Kaylee Eluvian <Kaylee.Eluvian@canterbury.ac.uk>
2016-07-18 13:58:01 +0100

    Initial creation of structure.

---
+++ ReadMe.md
@@ -0,0 +1,33 @@
+Solution structure
+
+Build
+	Files necessary to support building the project go in here.
+	XUnit execution configuration on the build server as also set here.
+	
+Product
+	Production
+		All the projects that make up the finished product go in here.
+		Examples:
+			MyProject.DomainObjects
+			MyProject.Services
+			MyProject.Systems
+			MyProject.Definitions
+			MyProject.IoC
+	Tests
+		Tertiary projects that support testing the product go in here.
+		Examples:
+			MyProject.Tests
+			MyProject.IntegrationTests
+
+ThirdParty
+	Documents
+		Code diagrams (DGML) and other supporting documents go here.
+	FxCop
+		FxCop project is here
+	SQL
+		Supporting SQL scripts go in here.
+		Examples:
+			Test data generation scripts
+			Schema creation scripts
+	StyleCop
+		StyleCop rules set is here.
---
+++ configurator.ps1
@@ -0,0 +1,16 @@
+﻿Clear-Host
+Write-Host "CCCU New Solution Configurator"
+Write-Host "-----------------------------"
+$projectName = Read-Host -Prompt "What is the name of this solution? > "
+if ($projectName.EndsWith(".sln"))
+{
+    $projectName = $projectName.Substring(0, $projectName.Length - 4).Trim()
+}
+
+$description = Read-Host -Prompt "Please describe this solution in a few words > "
+$gitOrigin = Read-Host -Prompt "What is the repo URL for this project in BitBucket? > "
+$wikiPage = Read-Host -Prompt "What is the Wiki URL for this solution? > "
+Read-Host -Prompt "Please make sure the solution is not open in Visual Studio or any other editors.  Press ENTER when ready."
+Write-Host "Thank you.  Configuring..."
+
+Rename-Item -Path ".\ThirdParty\Documents\ExampleProject.dgml" -NewName ($projectName + ".dgml")
