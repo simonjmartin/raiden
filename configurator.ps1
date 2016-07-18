57193fec403d68642a051b6865e234e6a100972f
Kaylee Eluvian <Kaylee.Eluvian@canterbury.ac.uk>
2016-07-18 16:43:41 +0100

    Added in a line so that files within projects that have been renamed are also recognised.

--- configurator.ps1
+++ configurator.ps1
@@ -42,7 +42,9 @@ function UpdateProject($projectPathFragment, $oldProjectName, $newProjectName){
     Replace -path (".\Product\" + $projectPathFragment + "\" + $oldProjectName + "\Properties\AssemblyInfo.cs") -val "ExampleProject" -repl $newProjectName
     Replace -path  (".\Product\" + $projectPathFragment + "\" + $oldProjectName + "\Class1.cs") -val "ExampleProject" -repl $newProjectName
     Rename-Item -Path (".\Product\" + $projectPathFragment + "\" + $oldProjectName + "\" + $oldProjectName + ".csproj") -NewName ($newProjectName + ".csproj")
-    
+    Replace -path  (".\Product\" + $projectPathFragment + "\" + $oldProjectName + "\" + $oldProjectName + ".csproj") -val "ExampleProject" -repl $projectName
+
+
     if (Test-Path (".\Product\" + $projectPathFragment + "\" + $oldProjectName + "\" + $oldProjectName + ".snk")){
         Rename-Item -Path (".\Product\" + $projectPathFragment + "\" + $oldProjectName + "\" + $oldProjectName + ".snk") -NewName ($newProjectName + "snk")
     }
